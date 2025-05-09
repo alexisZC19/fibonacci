04ba18dd2232ac96355376d4c1e8d66656ebf208
alexisZC19 <126813702+alexisZC19@users.noreply.github.com>
2025-05-10 01:48:21 +0200

    Update test_fibonacci.py

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,15 +1,13 @@
 import timeit
 from fibonacci import fibonacci
 
-def test_fibonacci () :
-    assert fibonacci (0) == 0
-    assert fibonacci (1) == 1
-    assert fibonacci (5) == 5
-    print ( " Pruebas pasadas ! " )
+def test_fibonacci():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(5) == 5
+    print("¡Pruebas pasadas! Zavaleta Cruz Jonathan ALexis")
 
-if __name__ == " __main__ " :
-    test_fibonacci ()
-    time = timeit . timeit ( " fibonacci (20) " ,
-          setup = " from fibonacci import fibonacci " ,
-          number =100)
-    print ( f " Tiempo : { time :.4 f } segundos " )
+if __name__ == "__main__":
+    test_fibonacci()
+    time = timeit.timeit("fibonacci(20)", setup="from fibonacci import fibonacci", number=100)
+    print(f"Tiempo de ejecución (100 iteraciones de fibonacci(20)): {time:.4f} segundos")
